dbe3d373506990c0428ff85222ae1ec36502f17c
misselzabet <lisa.kovalevskaya1999@gmail.com>
2025-05-18 03:07:53 +0200

    nnn

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import { PersistGate } from 'redux-persist/integration/react';
 import Loader from './components/Loader/Loader';
 
@@ -11,7 +12,7 @@ import './index.css';
 import App from './components/App';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
+    <Fragment>
         <Provider store={store}>
             {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
             <BrowserRouter>
