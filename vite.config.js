5fc48977b05a7d87ec9b133f28019543be9b68ce
misselzabet <lisa.kovalevskaya1999@gmail.com>
2025-05-18 02:40:48 +0200

    change vite2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    build: {
+        sourcemap: true,
     },
-  },
 });
