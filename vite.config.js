1275c04ca42a8f0e9daaef4cfd2ecc724e4e6cb6
misselzabet <lisa.kovalevskaya1999@gmail.com>
2025-05-18 02:48:03 +0200

    jjj

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        sourcemap: true,
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
+  },
 });
