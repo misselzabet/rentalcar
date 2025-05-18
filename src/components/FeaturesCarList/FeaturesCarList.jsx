ce4848e60d1d3cdc06ea5e86ecf96825515781dd
misselzabet <lisa.kovalevskaya1999@gmail.com>
2025-05-18 02:43:37 +0200

    added FeaturesCarList.jsx

---
+++ src/components/FeaturesCarList/FeaturesCarList.jsx
@@ -0,0 +1,20 @@
+import { FaRegCircleCheck } from 'react-icons/fa6';
+import s from './FeaturesCarList.module.css';
+
+const FeaturesCarList = ({ title, items }) => {
+    return (
+        <div className={s.listContainer}>
+            <h3 className={s.subtitle}>{title}</h3>
+            <ul className={s.list}>
+                {items.map((text, index) => (
+                    <li className={s.item} key={index}>
+                        <FaRegCircleCheck />
+                        <p>{text}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default FeaturesCarList;
